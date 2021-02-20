36a8048120cb1aa7cfe0ff98e72c3a3e3c56ab4f
topdev38 <gabriel.sabadac@yahoo.ro>
2021-02-20 23:40:02 +0200

    readme

--- company/tests.py
+++ company/tests.py
@@ -1,3 +1,23 @@
-from django.test import TestCase
+import pytest
 
-# Create your tests here.
+from .models import Company
+
+
+@pytest.fixture
+def defaul_data():
+    return {
+        'name': 'app'
+    }
+
+@pytest.mark.django_db
+def test_company_create():
+    Company.objects.create(name='ernie')
+    assert Company.objects.count() == 1
+
+@pytest.mark.django_db
+def test_company_creating(status_code, api_client):
+    url = reverse('login')
+    response = api_client.post(
+        url, data = default_data
+    )
+    assert  response.status_code == status_code
